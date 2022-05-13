059b154812a695549ba08501aadc521f34561f0b
Gasvn7 <morenogaston595@gmail.com>
2022-05-13 16:34:48 -0300

    Ya subido a Heroku y funcionando, actualizacion de archivo structure.sql

--- sql/structure.sql
+++ sql/structure.sql
@@ -1,157 +1,254 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: petitos
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.24-MariaDB
 
--- -----------------------------------------------------
--- Table `petitos`.`brands`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `petitos`.`brands` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `brand` VARCHAR(45) NULL DEFAULT NULL,
-  `created_at` TIMESTAMP NULL DEFAULT NULL,
-  `updated_at` TIMESTAMP NULL DEFAULT NULL,
-  `deleted_at` TIMESTAMP NULL DEFAULT NULL,
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `brands`
+--
+
+DROP TABLE IF EXISTS `brands`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `brands` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `brand` varchar(45) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 12
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_general_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `brands`
+--
 
--- -----------------------------------------------------
--- Table `petitos`.`categories`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `petitos`.`categories` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `category` VARCHAR(45) NULL DEFAULT NULL,
-  `created_at` TIMESTAMP NULL DEFAULT NULL,
-  `updated_at` TIMESTAMP NULL DEFAULT NULL,
-  `deleted_at` TIMESTAMP NULL DEFAULT NULL,
+LOCK TABLES `brands` WRITE;
+/*!40000 ALTER TABLE `brands` DISABLE KEYS */;
+INSERT INTO `brands` VALUES (1,'Royal Canin - Perro',NULL,NULL,NULL),(2,'Prop Plan - Perro',NULL,NULL,NULL),(3,'Eukanuba',NULL,NULL,NULL),(4,'Excellent - Perro',NULL,NULL,NULL),(5,'Dog Chow',NULL,NULL,NULL),(6,'Royal Canin - Gato',NULL,NULL,NULL),(7,'Prop Plan - Gato',NULL,NULL,NULL),(8,'Nutrique',NULL,NULL,NULL),(9,'Excellent - Gato',NULL,NULL,NULL),(10,'Cat Chow',NULL,NULL,NULL),(11,'Sin marca',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `brands` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(45) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 19
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_general_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `categories`
+--
 
--- -----------------------------------------------------
--- Table `petitos`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `petitos`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(16) NULL DEFAULT NULL,
-  `last_name` VARCHAR(20) NULL DEFAULT NULL,
-  `email` VARCHAR(255) NULL DEFAULT NULL,
-  `password` VARCHAR(250) NULL DEFAULT NULL,
-  `image` VARCHAR(45) NULL DEFAULT NULL,
-  `userRole` VARCHAR(100) NULL DEFAULT NULL,
-  `direction` VARCHAR(45) NULL DEFAULT NULL,
-  `created_at` TIMESTAMP NULL DEFAULT NULL,
-  `updated_at` TIMESTAMP NULL DEFAULT NULL,
-  `deleted_at` TIMESTAMP NULL DEFAULT NULL,
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Alimento para perros',NULL,NULL,NULL),(2,'Alimento para gatos',NULL,NULL,NULL),(3,'Juguetes para perros',NULL,NULL,NULL),(4,'Juguetes para gatos',NULL,NULL,NULL),(5,'Ropa para perros',NULL,NULL,NULL),(6,'Ropa para gatos',NULL,NULL,NULL),(7,'Camas para perros',NULL,NULL,NULL),(8,'Camas para gatos',NULL,NULL,NULL),(9,'Higiene para perros',NULL,NULL,NULL),(10,'Higiene para gatos',NULL,NULL,NULL),(11,'Estética para perros',NULL,NULL,NULL),(12,'Estética para gatos',NULL,NULL,NULL),(13,'Accesorios para perros',NULL,NULL,NULL),(14,'Accesorios para gatos',NULL,NULL,NULL),(15,'Collares para perros',NULL,NULL,NULL),(16,'Collares para gatos',NULL,NULL,NULL),(17,'Comederos y bebederos para perros',NULL,NULL,NULL),(18,'Comederos y bebederos para gatos',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lineorder`
+--
+
+DROP TABLE IF EXISTS `lineorder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lineorder` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `price` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `Line-ordercol` varchar(45) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 7
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_general_ci;
+  KEY `fk_Line-order_Products1_idx` (`product_id`),
+  KEY `fk_Line-order_Order1_idx` (`order_id`),
+  CONSTRAINT `fk_Line-order_Order1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `fk_Line-order_Products1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `lineorder`
+--
 
--- -----------------------------------------------------
--- Table `petitos`.`orders`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `petitos`.`orders` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  `status` INT NULL DEFAULT NULL,
-  `date` DATE NULL DEFAULT NULL,
-  `created_at` TIMESTAMP NULL DEFAULT NULL,
-  `updated_at` TIMESTAMP NULL DEFAULT NULL,
-  `deleted_at` TIMESTAMP NULL DEFAULT NULL,
+LOCK TABLES `lineorder` WRITE;
+/*!40000 ALTER TABLE `lineorder` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lineorder` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_order_user1_idx` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_order_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `petitos`.`users` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_general_ci;
+  KEY `fk_order_user1_idx` (`user_id`),
+  CONSTRAINT `fk_order_user1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `orders`
+--
 
--- -----------------------------------------------------
--- Table `petitos`.`sizes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `petitos`.`sizes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `size` VARCHAR(25) NULL DEFAULT NULL,
-  `created_at` TIMESTAMP NULL DEFAULT NULL,
-  `updated_at` TIMESTAMP NULL DEFAULT NULL,
-  `deleted_at` TIMESTAMP NULL DEFAULT NULL,
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `size_id` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `details` varchar(100) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_general_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_Products_Sizes1_idx` (`size_id`),
+  KEY `fk_Products_Brands1_idx` (`brand_id`),
+  KEY `fk_Products_Categories1_idx` (`category_id`),
+  CONSTRAINT `fk_Products_Brands1` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
+  CONSTRAINT `fk_Products_Categories1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  CONSTRAINT `fk_Products_Sizes1` FOREIGN KEY (`size_id`) REFERENCES `sizes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `products`
+--
 
--- -----------------------------------------------------
--- Table `petitos`.`products`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `petitos`.`products` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `size_id` INT NOT NULL,
-  `brand_id` INT NOT NULL,
-  `category_id` INT NOT NULL,
-  `name` VARCHAR(100) NULL DEFAULT NULL,
-  `price` INT NULL DEFAULT NULL,
-  `quantity` INT NULL DEFAULT NULL,
-  `details` VARCHAR(100) NULL DEFAULT NULL,
-  `image` VARCHAR(100) NULL DEFAULT NULL,
-  `updated_at` TIMESTAMP NULL DEFAULT NULL,
-  `created_at` TIMESTAMP NULL DEFAULT NULL,
-  `deleted_at` TIMESTAMP NULL DEFAULT NULL,
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,5,1,1,'Alimento Royal Canin para Perro Vegano',600,50,'Alimento bonito y rico para tu perrito bonito y chikito',NULL,NULL,NULL,NULL),(2,5,6,2,'Alimento Royal Canin para Gato Vegano',600,50,'Alimento bonito y rico para tu gatito bonito y chikito',NULL,NULL,NULL,NULL),(3,5,11,3,'Peluche de Goku anti-estres',166,15,'Peluche anti-estres con diseño de Goku de serie famosa china',NULL,NULL,NULL,NULL),(4,5,11,4,'Peluche de Vegeta anti-estres',166,15,'Peluche anti-estres con diseño de Vegeta de serie famosa turca',NULL,NULL,NULL,NULL),(5,2,11,5,'Camisa de leñador',375,30,'Camisa de leñador para perrito fachera, fecherita para tu perro leñador',NULL,NULL,NULL,NULL),(6,1,11,6,'Remera de Hello Kitty',1000,50,'Remera de Hello Kitty para tu gatito aesthetic',NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sizes`
+--
+
+DROP TABLE IF EXISTS `sizes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sizes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `size` varchar(25) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `fk_Products_Sizes1_idx` (`size_id` ASC) VISIBLE,
-  INDEX `fk_Products_Brands1_idx` (`brand_id` ASC) VISIBLE,
-  INDEX `fk_Products_Categories1_idx` (`category_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Products_Brands1`
-    FOREIGN KEY (`brand_id`)
-    REFERENCES `petitos`.`brands` (`id`),
-  CONSTRAINT `fk_Products_Categories1`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `petitos`.`categories` (`id`),
-  CONSTRAINT `fk_Products_Sizes1`
-    FOREIGN KEY (`size_id`)
-    REFERENCES `petitos`.`sizes` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 9
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_general_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `sizes`
+--
 
--- -----------------------------------------------------
--- Table `petitos`.`lineorder`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `petitos`.`lineorder` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `product_id` INT NOT NULL,
-  `order_id` INT NOT NULL,
-  `price` INT NULL DEFAULT NULL,
-  `quantity` INT NULL DEFAULT NULL,
-  `Line-ordercol` VARCHAR(45) NULL DEFAULT NULL,
-  `created_at` TIMESTAMP NULL DEFAULT NULL,
-  `updated_at` TIMESTAMP NULL DEFAULT NULL,
-  `deleted_at` TIMESTAMP NULL DEFAULT NULL,
+LOCK TABLES `sizes` WRITE;
+/*!40000 ALTER TABLE `sizes` DISABLE KEYS */;
+INSERT INTO `sizes` VALUES (1,'Pequeño',NULL,NULL,NULL),(2,'Mediano',NULL,NULL,NULL),(3,'Grande',NULL,NULL,NULL),(4,'Muy Grande',NULL,NULL,NULL),(5,'No tiene',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `sizes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(16) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(250) DEFAULT NULL,
+  `image` varchar(45) DEFAULT NULL,
+  `userRole` varchar(100) DEFAULT NULL,
+  `direction` varchar(45) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_Line-order_Products1_idx` (`product_id` ASC) VISIBLE,
-  INDEX `fk_Line-order_Order1_idx` (`order_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Line-order_Order1`
-    FOREIGN KEY (`order_id`)
-    REFERENCES `petitos`.`orders` (`id`),
-  CONSTRAINT `fk_Line-order_Products1`
-    FOREIGN KEY (`product_id`)
-    REFERENCES `petitos`.`products` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_general_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Primer','Admin','elprimeradmin555@gmail.com','adminadminadmin3','no-image','Admin',NULL,NULL,NULL,NULL),(2,'Usuario','Ejemplo','usamecomoejemplo@gmail.com','soyunejemploejemplar','no-image','Client',NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'petitos'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-13 13:52:17
